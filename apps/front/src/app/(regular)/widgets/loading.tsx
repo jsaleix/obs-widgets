c89f830b950bf948496f1f2b235eb8269c7d91c3
julian-saleix <julian.saleix@gmail.com>
2024-03-03 17:49:41 +0100

    chore: Adding loader component

---
+++ apps/front/src/app/(regular)/widgets/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from "@/components/misc/loader";
+
+export default function Loading() {
+    return (
+        <div className="w-full py-10 flex items-center justify-center">
+            <Loader />
+        </div>
+    );
+}
