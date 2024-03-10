2bda9e31bfdb656a18f967369afce216eadbfa3d
julian-saleix <julian.saleix@gmail.com>
2024-03-10 22:38:16 +0100

    chore: FIREBASE_PRIVATE_KEY is now a stringified json bcause the key is long & multiline

--- apps/front/src/lib/firebase/config.ts
+++ apps/front/src/lib/firebase/config.ts
@@ -4,9 +4,11 @@ import { credential } from "firebase-admin";
 import { initializeApp, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string);
+
 export const firebaseConfig = {
     credential: credential.cert({
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         projectId: process.env.FIREBASE_PROJECT_ID,
     }),
