027f91deccb2747cc44f30f92fdde7983fe323e8
julian-saleix <julian.saleix@gmail.com>
2024-03-10 22:53:47 +0100

    fix: defining authOptions before getServerAuthSession seems to fix url

--- apps/front/src/lib/auth/index.ts
+++ apps/front/src/lib/auth/index.ts
@@ -5,18 +5,6 @@ import { Adapter } from "next-auth/adapters";
 import { firebaseConfig } from "../firebase/config";
 import twitchProvider from "./providers/twitch-provider";
 
-export const getServerAuthSession = () => getServerSession(authOptions);
-
-export async function checkPermission(counterId: string) {
-    const session = await getServerAuthSession();
-    if (!session) throw new Error("Not authenticated.");
-    const isAllowed = await counterService.isAllowedToEdit(
-        counterId,
-        session.user.id!
-    );
-    if (!isAllowed) throw new Error("Not allowed.");
-}
-
 export const authOptions: NextAuthOptions = {
     adapter: FirestoreAdapter(firebaseConfig) as Adapter,
     session: {
@@ -32,3 +20,15 @@ export const authOptions: NextAuthOptions = {
     providers: [twitchProvider],
     secret: process.env.NEXTAUTH_SECRET,
 };
+
+export const getServerAuthSession = () => getServerSession(authOptions);
+
+export async function checkPermission(counterId: string) {
+    const session = await getServerAuthSession();
+    if (!session) throw new Error("Not authenticated.");
+    const isAllowed = await counterService.isAllowedToEdit(
+        counterId,
+        session.user.id!
+    );
+    if (!isAllowed) throw new Error("Not allowed.");
+}
