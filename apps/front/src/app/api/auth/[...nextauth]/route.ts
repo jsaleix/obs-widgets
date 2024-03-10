5ca69bf88b01e5ff550931655d0c406eb3b98203
julian-saleix <julian.saleix@gmail.com>
2024-03-10 22:17:53 +0100

    fix: authOptions export in route.ts that trigger fail at build time

--- apps/front/src/app/api/auth/[...nextauth]/route.ts
+++ apps/front/src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,5 @@
-import twitchProvider from "@/lib/auth/providers/twitch-provider";
-import { firebaseConfig } from "@/lib/firebase/config";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
-
-export const authOptions: NextAuthOptions = {
-    adapter: FirestoreAdapter(firebaseConfig) as Adapter,
-    session: {
-        strategy: "jwt",
-    },
-    callbacks: {
-        async session({ session, token }) {
-            if (!token) return session;
-            session.user.id = token.sub;
-            return session;
-        },
-    },
-    providers: [twitchProvider],
-    secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
