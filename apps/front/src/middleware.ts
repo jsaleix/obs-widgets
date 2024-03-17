9537b15ec72b5498c90eed049d413690cd0f81f0
julian-saleix <julian.saleix@gmail.com>
2024-03-17 22:21:08 +0100

    fix: protected paths not working in middleware

--- apps/front/src/middleware.ts
+++ apps/front/src/middleware.ts
@@ -1,7 +1,5 @@
-import { protectedPaths } from "./lib/config/routes";
-
 export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: protectedPaths,
+    matcher: ["/widgets", "/admin"],
 };
